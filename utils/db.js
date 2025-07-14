74fe3b8dcba80ae1c94cf624ddeb25da1c4d9651
Rajashri <171247501+Rajashrri@users.noreply.github.com.>
2025-07-14 13:44:14 +0530

    database

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const URI = "mongodb://127.0.0.1:27017/task-manager";
+const URI = "mongodb+srv://rajashrichougule:ENzLAEIi7RA7eZkE@cluster0.luemvjm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 // const URI = "mongodb+srv://digihost2021:E1IBiNmZzhWtKDyB@cluster0.c1qye.mongodb.net/test";
 // mongoose.connect(URI);
 
